8dfa88a5b9d2ed0450339d1e8975bbbfdce12ab3
CarlosCente <31189121+CarlosCente@users.noreply.github.com>
2022-02-13 12:30:22 +0100

    Subida inicial
    Subida inicial

---
+++ src/test/java/com/gestdocu/GestdocuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestdocu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestdocuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
